807823ff3ce9b8132cca71f2e1dae6486d2ffff4
Angel Delgado <angel.delgado01@utrgv.edu>
2019-06-19 01:01:14 -0500

    Edit: Bottom Navigation Bar
    Finished setting up the bottom navigation bar with fragments. Now we just have to edit each fragment class depending on what we want each page to do .

--- ContactQR/app/src/main/java/com/example/contactqr/MainActivity.java
+++ ContactQR/app/src/main/java/com/example/contactqr/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.contactqr;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializes Bottom Nav
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        bottomNav.setSelectedItemId(R.id.nav_code);
+
+        // Starts a New Fragment When App is Loaded
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CodeFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener;
+
+    {
+        navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+
+                    case R.id.nav_edit:
+                        selectedFragment = new EditFragment();
+                        break;
+                    case R.id.nav_code:
+                        selectedFragment = new CodeFragment();
+                        break;
+                    case R.id.nav_scan:
+                        selectedFragment = new ScanFragment();
+                        break;
+
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+
+            }
+        };
+    }
+
 }
