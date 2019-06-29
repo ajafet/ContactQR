e928e03d25ab78724baab0133db6dac97cd67e40
Angel Delgado <angel.delgado01@utrgv.edu>
2019-06-29 00:35:44 -0500

    All Features Implemented
    All features have been implemented.

--- ContactQR/app/src/main/java/com/example/contactqr/EditFragment.java
+++ ContactQR/app/src/main/java/com/example/contactqr/EditFragment.java
@@ -1,6 +1,7 @@
 package com.example.contactqr;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,9 +9,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import java.io.File;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import android.support.v4.app.FragmentTransaction;
+
+import android.app.Activity;
 
 public class EditFragment extends Fragment {
+
+    private static final String FILE_NAME = "info.txt";
 
     @Nullable
     @Override
@@ -18,20 +38,106 @@ public class EditFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_edit, container, false);
 
-        Button btnOpen = (Button) view.findViewById(R.id.edit_button);
+        LinearLayout btnOpen = (LinearLayout) view.findViewById(R.id.edit_QR);
         btnOpen.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
             Intent in = new Intent(getActivity(), InputActivity.class);
-            startActivity(in);
+            startActivityForResult(in, 10001);
 
             }
 
         });
 
+        //
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        ImageView image = (ImageView) view.findViewById(R.id.image);
+
+        try {
+
+            if (getInfo() == null)  {
+
+                image.setImageDrawable(getResources().getDrawable(R.drawable.ic_local_hospital_black_24dp));
+
+            }
+            else {
+
+                BitMatrix bitMatrix = multiFormatWriter.encode(getInfo(), BarcodeFormat.QR_CODE, 200, 200);
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+                image.setImageBitmap(bitmap);
+
+            }
+
+        } catch (WriterException e) {
+
+            e.printStackTrace();
+
+        }
+        //
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == 10001) && (resultCode == Activity.RESULT_OK)) {
+
+            Fragment frg = null;
+            frg = getFragmentManager().findFragmentById(R.id.fragment_container);
+            final FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.detach(frg);
+            ft.attach(frg);
+            ft.commit();
+
+        }
+
+    }
+
+    public String getInfo () {
+
+        FileInputStream fis = null;
+
+        try {
+
+            fis = getContext().openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            String info_text = br.readLine();
+            return info_text;
+
+        }catch (FileNotFoundException e) {
+
+            return null;
+
+        }catch (IOException e) {
+
+            return null;
+
+        }finally {
+
+            if (fis != null) {
+
+                try{
+
+                    fis.close();
+
+                }catch (IOException e) {
+
+                    return null;
+
+                }
+
+            }
+
+        }
+
     }
 
 }
--- ContactQR/app/src/main/java/com/example/contactqr/InputActivity.java
+++ ContactQR/app/src/main/java/com/example/contactqr/InputActivity.java
@@ -1,60 +1,190 @@
 package com.example.contactqr;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import android.support.v4.app.Fragment;
+
+import android.app.AlertDialog;
+
 
 
 public class InputActivity extends AppCompatActivity {
 
-    EditText text;
-    Button gen_btn;
-    ImageView image;
-    String text2QR;
+    private static final String FILE_NAME = "info.txt";
+    private EditText name;
+    private EditText phone;
+    private Button gen_btn;
+    private ImageView image;
+    private String text2QR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
-        text = (EditText) findViewById(R.id.text);
+        name = (EditText) findViewById(R.id.name);
+        phone = (EditText) findViewById(R.id.phone);
         gen_btn = (Button) findViewById(R.id.gen_btn);
         image = (ImageView) findViewById(R.id.image);
+
+        loadInfo();
+
+        //set listeners
+        name.addTextChangedListener(textWatcher);
+        phone.addTextChangedListener(textWatcher);
+
+        // run once to disable if empty
+        checkFieldsForEmptyValues();
 
         gen_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                text2QR = text.getText().toString().trim();
-                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                text2QR = name.getText().toString().trim() + ":" + phone.getText().toString().trim();
+                saveInfo();
 
-                try {
+                setResult(Activity.RESULT_OK);
 
-                    BitMatrix bitMatrix = multiFormatWriter.encode(text2QR, BarcodeFormat.QR_CODE, 200, 200);
-                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-                    image.setImageBitmap(bitmap);
+                finish();
 
-                }
-                catch (WriterException e) {
+            }
+        });
+
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3)
+        {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            checkFieldsForEmptyValues();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    };
+
+    private void checkFieldsForEmptyValues(){
+
+        Button b = (Button) findViewById(R.id.gen_btn);
+        String s1 = name.getText().toString();
+        String s2 = phone.getText().toString();
+
+        if(s1.equals("") && s2.equals(""))
+        {
+            b.setEnabled(false);
+        }
+
+        else if(!s1.equals("") && s2.equals("")){
+            b.setEnabled(false);
+        }
+
+        else if(!s2.equals("") && s1.equals(""))
+        {
+            b.setEnabled(false);
+        }
+
+        else
+        {
+            b.setEnabled(true);
+        }
+    }
+
+    public void saveInfo () {
+
+        FileOutputStream fos = null;
+
+        try {
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text2QR.getBytes());
+
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+            fos.close();
+
+        }catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+
+        }catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void loadInfo () {
+
+        FileInputStream fis = null;
+
+        try {
+
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            String info = br.readLine();
+            String [] split = info.split(":");
+
+            name.setText(split[0]);
+            phone.setText(split[1]);
+
+        }catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+
+        }catch (IOException e) {
+
+            e.printStackTrace();
+
+        }finally {
+
+            if (fis != null) {
+
+                try{
+
+                    fis.close();
+
+                }catch (IOException e) {
 
                     e.printStackTrace();
 
                 }
 
             }
-        });
+
+        }
 
     }
+
 }
